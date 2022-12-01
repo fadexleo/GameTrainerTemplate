f410e40bfed4a17b03c071f1ad01587d71d02aaf
Andrew Tate <Andrew Tate@DESKTOP-H4JBOTN>
2022-12-01 08:01:13 +0000

    Add project files.

---
+++ LeosMenuTemplate/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace LeosMenuTemplate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.title = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.title);
+            this.panel1.Location = new System.Drawing.Point(46, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(255, 374);
+            this.panel1.TabIndex = 0;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(15, 336);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 20);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Misc >";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(15, 296);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 20);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Weapon >";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(15, 256);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Player >";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(15, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Spawner >";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(15, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Story Mode >";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(13, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Online >";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(13, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ESP >";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(13, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Aimbot >";
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.Black;
+            this.title.Location = new System.Drawing.Point(15, 16);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(154, 20);
+            this.title.TabIndex = 0;
+            this.title.Text = "Leo\'s Cheat Menu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label title;
+    }
+}
+
---
+++ LeosMenuTemplate/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ezOverLay;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Drawing.Drawing2D;
+
+namespace LeosMenuTemplate
+{
+    public partial class Form1 : Form
+    {
+        ez ez = new ez();
+
+        // initiate your memory dll here
+
+        // global variables
+        bool menuOpen = true;
+        int menuItemSelected = 0;
+        int menuPageSelected = 0;
+        string[,] menuItems = new string[9, 8] { 
+            { "Aimbot >", "ESP >", "Online >", "Story Mode >", "Spawner >", "Player >", "Weapon >", "Misc >" }, 
+            { "Enable", "Fov", "Smoothing", "RCS", "Feature 4", "Feature 5", "Feature 6", "Feature 7" }, 
+            { "Enable", "Box", "Cornered", "Team", "Tracers", "Feature 5", "Feature 6", "Feature 7" }, 
+            { "Enable", "Feature 1", "Feature 2", "Feature 3", "Feature 4", "Feature 5", "Feature 6", "Feature 7" }, 
+            { "Enable", "Feature 1", "Feature 2", "Feature 3", "Feature 4", "Feature 5", "Feature 6", "Feature 7" }, 
+            { "Enable", "Feature 1", "Feature 2", "Feature 3", "Feature 4", "Feature 5", "Feature 6", "Feature 7" },
+            { "Enable", "Feature 1", "Feature 2", "Feature 3", "Feature 4", "Feature 5", "Feature 6", "Feature 7" },
+            { "Enable", "Feature 1", "Feature 2", "Feature 3", "Feature 4", "Feature 5", "Feature 6", "Feature 7" },
+            { "Enable", "Feature 1", "Feature 2", "Feature 3", "Feature 4", "Feature 5", "Feature 6", "Feature 7" }
+        };
+    
+
+
+        [DllImport("User32.dll")]
+        public static extern short GetAsyncKeyState(Keys vKey);
+
+        struct Offsets // put your offsets in here
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            ez.SetInvi(this);
+            ez.DoStuff("Counter-Strike: Global Offensive - Direct3D 9", this); // replace "Counter-Strike: Global Offensive - Direct3D 9" with game name
+
+            label1.Text = menuItems[0, 0];
+            label2.Text = menuItems[0, 1];
+            label3.Text = menuItems[0, 2];
+            label4.Text = menuItems[0, 3];
+            label5.Text = menuItems[0, 4];
+            label6.Text = menuItems[0, 5];
+            label7.Text = menuItems[0, 6];
+            label8.Text = menuItems[0, 7];
+
+            Thread menuControls = new Thread(Menu) { IsBackground = true };
+            menuControls.Start();
+        }
+
+        void Menu()
+        {
+            while (true)
+            {
+                if (GetAsyncKeyState(Keys.Insert) < 0)
+                {
+                    menuOpen = !menuOpen;
+                    Thread.Sleep(150);
+                }
+
+                if (menuOpen == true) 
+                { 
+                    panel1.Visible = true; 
+
+                    if (GetAsyncKeyState(Keys.Down) < 0)
+                    {
+                        var item = menuSelector(menuItemSelected);
+                        item.ForeColor = Color.Black;
+                        menuItemSelected += 1;
+                        if (menuItemSelected > 7) { menuItemSelected = 0; } else if (menuItemSelected < 0) { menuItemSelected = 7; }
+                        item = menuSelector(menuItemSelected);
+                        item.ForeColor = Color.Yellow;
+                        Thread.Sleep(150);
+
+                    }
+
+                    if (GetAsyncKeyState(Keys.Up) < 0)
+                    {
+                        var item = menuSelector(menuItemSelected);
+                        item.ForeColor = Color.Black;
+                        menuItemSelected -= 1;
+                        if (menuItemSelected > 7) { menuItemSelected = 0; } else if (menuItemSelected < 0) { menuItemSelected = 7; }
+                        item = menuSelector(menuItemSelected);
+                        item.ForeColor = Color.Yellow;
+                        Thread.Sleep(150);
+                    }
+
+                    if (GetAsyncKeyState(Keys.Enter) < 0)
+                    {
+                        menuPageSelected = menuItemSelected + 1;
+                        label1.Text = menuItems[menuPageSelected, 0];
+                        label2.Text = menuItems[menuPageSelected, 1];
+                        label3.Text = menuItems[menuPageSelected, 2];
+                        label4.Text = menuItems[menuPageSelected, 3];
+                        label5.Text = menuItems[menuPageSelected, 4];
+                        label6.Text = menuItems[menuPageSelected, 5];
+                        label7.Text = menuItems[menuPageSelected, 6];
+                        label8.Text = menuItems[menuPageSelected, 7];
+
+                        menuSelector(menuItemSelected).ForeColor = Color.Black;
+                        menuItemSelected = 0;
+                        menuSelector(menuItemSelected).ForeColor = Color.Yellow;
+
+                        Thread.Sleep(150);
+                    }
+
+                    if (GetAsyncKeyState(Keys.Back) < 0)
+                    {
+                        label1.Text = menuItems[0, 0];
+                        label2.Text = menuItems[0, 1];
+                        label3.Text = menuItems[0, 2];
+                        label4.Text = menuItems[0, 3];
+                        label5.Text = menuItems[0, 4];
+                        label6.Text = menuItems[0, 5];
+                        label7.Text = menuItems[0, 6];
+                        label8.Text = menuItems[0, 7];
+
+                        menuSelector(menuItemSelected).ForeColor = Color.Black;
+                        menuItemSelected = 0;
+                        menuSelector(menuItemSelected).ForeColor = Color.Yellow;
+
+                        Thread.Sleep(150);
+                    }
+
+                } 
+                else 
+                { 
+                    panel1.Visible = false; 
+                }
+
+
+                Thread.Sleep(10);
+            }
+            
+        }
+
+        public Label menuSelector(int selected)
+        {
+            if (selected == 0) { return label1; } else if (selected == 1) { return label2; } else if (selected == 2) { return label3; } else if (selected == 3) { return label4; } else if (selected == 4) { return label5; } else if (selected == 5) { return label6; } else if (selected == 6) { return label7; } else if (selected == 7) { return label8; } else { return label1; }
+        }
+
+
+    }
+}
